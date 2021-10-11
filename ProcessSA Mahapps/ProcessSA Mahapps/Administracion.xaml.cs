3efcb389e3733c3df0c6443e6f6ff81a5c233e29
Benjamín Vásquez <b.vasquezd@duocuc.cl>
2021-10-11 16:23:01 -0300

    App Escritorio

---
+++ ProcessSA Mahapps/ProcessSA Mahapps/Administracion.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ProcessSA_Mahapps
+{
+    /// <summary>
+    /// Lógica de interacción para Administracion.xaml
+    /// </summary>
+    public partial class Administracion : Page
+    {
+        public Administracion()
+        {
+            InitializeComponent();
+        }
+
+        MetroWindow window = Application.Current.MainWindow as MetroWindow;
+
+        private void BtnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Usuarios());
+        }
+
+        private void BtnTareas_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Tareas());
+        }
+    }
+}
---
+++ ProcessSA Mahapps/ProcessSA Mahapps/Tareas.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcessSA_Mahapps
+{
+    /// <summary>
+    /// Lógica de interacción para Tareas.xaml
+    /// </summary>
+    public partial class Tareas : Page
+    {
+        public Tareas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtras_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Administracion());
+        }
+    }
+}
